273fc037a4defdb513b7b5b57d1595fae96d6f14
Allison Duncan <allison.duncan426@gmail.com>
2024-05-07 16:34:34 -0500

    act 01

--- ch-01/ex1.01.go
+++ ch-01/ex1.01.go
@@ -8,8 +8,24 @@ import (
 )
 
 func Main() {
+	ex01()
+	act01()
+}
+
+func ex01() {
 	rand.Seed(time.Now().UnixNano())
 	r := rand.Intn(5) + 1
 	stars := strings.Repeat("*", r)
 	fmt.Println(stars)
+}
+
+func act01() {
+	var (
+		fname          string = "Allison"
+		lname          string = "Duncan"
+		age            int    = 29
+		peanut_allergy bool   = false
+	)
+
+	fmt.Println(fname, " ", lname, " ", age, " ", peanut_allergy)
 }
